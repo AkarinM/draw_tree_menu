1e45bb755468f67e892d6cbf753a0a37f9622730
akarin <mihailmoro96@yandex.ru>
2024-04-11 22:57:50 +0300

    Модели, Начальная настройка админки, миграции

--- tree_menu/menu/admin.py
+++ tree_menu/menu/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from menu.models import MenuName, MenuItem
+
+
+# admin.site.register(MenuName)
+
+
+@admin.register(MenuName)
+class MenuNameAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    pass
--- tree_menu/menu/models.py
+++ tree_menu/menu/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuName(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    items = models.ManyToManyField('MenuItem', through='MenuNameItem', related_name='menu')
+
+    class Meta:
+        ordering = ['pk']
+
+    def __str__(self):
+        return f'{self.pk}: {self.name}'
+
+
+class MenuItem(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+
+    class Meta:
+        ordering = ['pk']
+
+    def __str__(self):
+        return f'{self.pk}: {self.name}'
+
+
+class MenuNameItem(models.Model):
+    menu = models.ForeignKey(MenuName, on_delete=models.deletion.CASCADE)
+    item = models.ForeignKey(MenuItem, on_delete=models.deletion.CASCADE)
+    level = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['pk', 'level']
